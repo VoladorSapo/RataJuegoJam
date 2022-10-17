c03923d067d28797a3639269898a2ed5fb89d880
alberto662 <alberto.fierro03@gmail.com>
2022-10-17 21:02:07 +0200

    cambios de script de enemigos

--- Rat/Assets/Code/EnemyMovement.cs
+++ Rat/Assets/Code/EnemyMovement.cs
@@ -10,8 +10,9 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpeed = 2f; // velocidad de movimiento 
     public int waypointIndex = 0; // index actual
     int direction; // 1 direccion de ida 0 de vuelta
+    private bool enemyStop;
+    public float waitSeconds;
 
-    //
     private void Awake()
     {
         direction = 1;
@@ -20,6 +21,7 @@ public class EnemyMovement : MonoBehaviour
         {
             waypoints[i] = waypointsGO.GetChild(i);
         }
+        enemyStop = false;
     }
     // Use this for initialization
     private void Start()
@@ -27,14 +29,23 @@ public class EnemyMovement : MonoBehaviour
         direction = 1;
         objective = waypoints[1];
     }
-
+    public void stopEnemy()
+    {
+        StartCoroutine(enemyStopCo());
+    }
+    IEnumerator enemyStopCo()
+    {
+        enemyStop = true;
+        yield return new WaitForSeconds(waitSeconds);
+        enemyStop = false;
+    }
     // Update is called once per frame
     private void Update()
     {
         Vector2 dir = objective.position - transform.position;
-        transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
+        if (!enemyStop){transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);}
         if (Vector2.Distance(transform.position, objective.position) <= 0.05f)
-        {
+        { 
             NextWayPoint();
         }
         if (waypoints.Length == waypointIndex - 1)
