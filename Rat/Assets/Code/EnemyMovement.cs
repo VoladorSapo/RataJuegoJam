83eabed9db39a789205e463dfb450af6570c0415
VoladorSapo <voladorsapo@gmail.com>
2022-10-18 19:27:27 +0200

    Cambios error

--- Rat/Assets/Code/EnemyMovement.cs
+++ Rat/Assets/Code/EnemyMovement.cs
@@ -10,9 +10,8 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpeed = 2f; // velocidad de movimiento 
     public int waypointIndex = 0; // index actual
     int direction; // 1 direccion de ida 0 de vuelta
-    private bool enemyStop;
-    public float waitSeconds;
 
+    //
     private void Awake()
     {
         direction = 1;
@@ -21,7 +20,6 @@ public class EnemyMovement : MonoBehaviour
         {
             waypoints[i] = waypointsGO.GetChild(i);
         }
-        enemyStop = false;
     }
     // Use this for initialization
     private void Start()
@@ -29,23 +27,14 @@ public class EnemyMovement : MonoBehaviour
         direction = 1;
         objective = waypoints[1];
     }
-    public void stopEnemy()
-    {
-        StartCoroutine(enemyStopCo());
-    }
-    IEnumerator enemyStopCo()
-    {
-        enemyStop = true;
-        yield return new WaitForSeconds(waitSeconds);
-        enemyStop = false;
-    }
+
     // Update is called once per frame
     private void Update()
     {
         Vector2 dir = objective.position - transform.position;
-        if (!enemyStop){transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);}
+        transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
         if (Vector2.Distance(transform.position, objective.position) <= 0.05f)
-        { 
+        {
             NextWayPoint();
         }
         if (waypoints.Length == waypointIndex - 1)
